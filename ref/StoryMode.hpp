052a8d66334f2c9b69ee311fbedc2d95c5af8bc1
miyehn <rainduym@gmail.com>
2019-09-07 00:33:40 -0400

    stripped away old game from base code. start fresh

--- ref/StoryMode.hpp
+++ ref/StoryMode.hpp
@@ -18,7 +18,25 @@ struct StoryMode : Mode {
 	void enter_scene();
 
 	//------ story state -------
-  bool state_flag = false;
+	enum {
+		Dunes,
+		Oasis,
+		Hill
+	} location = Dunes;
+	bool have_stone = false;
+	bool added_stone = false;
+	struct {
+		bool first_visit = true;
+		bool wont_leave = false;
+	} dunes;
+	struct {
+		bool first_visit = true;
+		bool took_stone = false;
+	} oasis;
+	struct {
+		bool first_visit = true;
+		bool added_stone = false;
+	} hill;
 	
 	glm::vec2 view_min = glm::vec2(0,0);
 	glm::vec2 view_max = glm::vec2(256, 224);
